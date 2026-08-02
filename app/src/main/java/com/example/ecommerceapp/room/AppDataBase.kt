6fb552762b2309fdde75f2adfda56c4ba58500ac
haithamshetia <ahaitham656@gmail.com>
2026-08-02 21:03:35 +0300

    Adding Room database

---
+++ app/src/main/java/com/example/ecommerceapp/room/AppDataBase.kt
@@ -0,0 +1,8 @@
+package com.example.ecommerceapp.room
+
+import androidx.room.Database
+import com.example.ecommerceapp.model.Product
+
+@Database(entities = [Product::class], version = 1, exportSchema = false)
+abstract class AppDataBase {
+}
--- app/src/main/java/com/example/ecommerceapp/room/AppDateBase.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerceapp.room;
-
-import androidx.room.Database;
-
-import com.example.ecommerceapp.model.Product;
-
-@Database(entities = [Product::class] , version = 1 , exportSchema = false)
-abstract public class AppDateBase {
-}
